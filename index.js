9b47d6d1d8d8ce169b0f9f0582e507fdac1932b6
reecedantin <reecedantin@yahoo.com>
2018-08-13 15:49:42 -0400

    moved to custom solution for working with openhab

--- index.js
+++ index.js
@@ -1,113 +1,63 @@
-// OpenHAB 1 Platform for HomeBridge
-// Written by Tommaso Marchionni
-// Based on many of the other HomeBridge platform modules
-//
-// Revisions:
-//
-// 17 October 2015 [tommasomarchionni]
-// - Initial release
-//
-// 25 October 2015 [tommasomarchionni]
-// - Added WS listener and new OOP structure
-//
-// 5 December 2015 [tommasomarchionni]
-// - Adapted for new plugin system
-//
-// 14 December 2015 [tommasomarchionni]
-// - Added temperature sensor item
-// - Added switch item,lightbulb item and fan item
-//
-// 16 December 2015 [tommasomarchionni]
-// - Added the possibility to load every type of sitemap
-// - Added skipItem in customAttr to avoid to load item in Homekit catalog
-// - Added outlet item
-//
-// 17 December 2015 [tommasomarchionni]
-// - Added contact item
-//
-// 11 February 2016 [tommasomarchionni]
-// - Experimental support for Thermostat
-//
-// 12 February 2016 [tommasomarchionni]
-// - Added support for humidity in Thermostat
-//
-// Rollershutter is tested with this binding in OpenHAB:
-// command=SWITCH_MULTILEVEL,invert_percent=true,invert_state=false"
-// When you attempt to add a device, it will ask for a "PIN code".
-// The default code for all HomeBridge accessories is 031-45-154.
-//
+//HIFI2 PLATFORM
 
-'use strict';
+var request = require("request")
+var HIFIOutput = require("./lib/output")
+var Service, Characteristic, Accessory, uuid, hap;
 
-//////// LIBS /////////
+var powerIO = [];
+var volumeIO = [];
+var sourceIO = [];
 
-var Homebridge, Accessory;
-var request = require("request");
-var ItemFactory = require('./libs/ItemFactory.js');
-var Utility = require('./libs/Utility.js');
 
-//////// EXPORTS /////////
-
-module.exports = function(homebridge) {
+/* Register the plugin with homebridge */
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
     Accessory = homebridge.hap.Accessory;
-    Homebridge = homebridge;
+    uuid = homebridge.hap.uuid;
+    hap = homebridge.hap;
 
-    Utility.addSupportTo(ItemFactory.AbstractItem, Accessory);
-    Utility.addSupportTo(ItemFactory.SwitchItem, ItemFactory.AbstractItem);
-    Utility.addSupportTo(ItemFactory.LightbulbItem, ItemFactory.SwitchItem);
-    Utility.addSupportTo(ItemFactory.FanItem, ItemFactory.SwitchItem);
-    Utility.addSupportTo(ItemFactory.OutletItem, ItemFactory.SwitchItem);
-    Utility.addSupportTo(ItemFactory.DimmerItem, ItemFactory.AbstractItem);
-    Utility.addSupportTo(ItemFactory.RollershutterItem, ItemFactory.AbstractItem);
-    Utility.addSupportTo(ItemFactory.TemperatureSensorItem, ItemFactory.AbstractItem);
-    Utility.addSupportTo(ItemFactory.NumberItem, ItemFactory.TemperatureSensorItem);
-    Utility.addSupportTo(ItemFactory.ContactItem, ItemFactory.AbstractItem);
-    Utility.addSupportTo(ItemFactory.MotionSensorItem, ItemFactory.ContactItem);
-    Utility.addSupportTo(ItemFactory.LightSensorItem, ItemFactory.TemperatureSensorItem); //TODO ricontrollare
-    Utility.addSupportTo(ItemFactory.ThermostatItem, ItemFactory.AbstractItem);
-
-    homebridge.registerPlatform("homebridge-openhab-AUDIO", "openHAB-AUDIO", OpenHABPlatform);
-};
-
-//////// PLATFORM /////////
-
-function OpenHABPlatform(log, config){
-    this.log      = log;
-    this.user     = config["user"];
-    this.password = config["password"];
-    this.host     = config["host"];
-    this.port     = config["port"];
-    this.inputs   = config["inputs"];
-    this.outputs  = config["outputs"];
-    this.protocol = "http";
-    this.sitemap  = "demo";
-    if (typeof config["useLabelForName"] != 'undefined') {
-        this.useLabelForName = config["useLabelForName"];
-    }
-    if (typeof config["sitemap"] != 'undefined') {
-        this.sitemap = config["sitemap"];
-    }
-    if (typeof config["customAttrs"] != 'undefined') {
-        this.customAttrs = config["customAttrs"];
-    } else {
-        this.customAttrs = [];
-    }
+    homebridge.registerPlatform("homebridge-HIFI2", "HIFI2", HIFIPlatform);
 }
 
-OpenHABPlatform.prototype.accessories = function(callback) {
-    var that = this;
-    this.log("Platform - Fetching OpenHAB-AUDIO devices.");
-    var itemFactory = new ItemFactory.Factory(this,Homebridge);
-    var url = itemFactory.sitemapUrl();
-    this.log("Platform - Connecting to " + url);
+function HIFIPlatform(log, config) {
+    this.log = log;
+    this.host = config.host;
+    this.port = config.port;
+    this.sitemap = config.sitemap;
+    this.inputs = config.inputs;
+    this.outputs = config.outputs;
+}
+
+HIFIPlatform.prototype.accessories = function (callback) {
+
+    url = "http://" + this.host + ":" + this.port + "/rest/sitemaps/" + this.sitemap + "?type=json";
+
     request.get({
         url: url,
         json: true
     }, function(err, response, json) {
         if (!err && response.statusCode == 200) {
-            callback(itemFactory.parseSitemap(json));
+            if (Array.isArray(json.homepage.widget)) {
+                var results = [];
+                for(var output in this.outputs) {
+                    var source = json.homepage.widget[3 * output].item
+                    var power = json.homepage.widget[3 * output + 1].item
+                    var volume = json.homepage.widget[3 * output + 2].item
+                    sourceIO.push(source.state)
+                    powerIO.push(power.state === "ON")
+                    volumeIO.push(volume.state)
+                    results.push(new HIFIOutput(hap, this.log, this.outputs[output], this.inputs, source, power, volume, sourceIO[output], powerIO[output], volumeIO[output]));
+                }
+                this.log(sourceIO)
+                this.log(powerIO)
+                this.log(volumeIO)
+                callback(results)
+            } else {
+                this.log("Error parsing config file");
+            }
         } else {
-            that.log("Platform - There was a problem connecting to OpenHAB.");
+            console.log("Platform - There was a problem connecting to OpenHAB.");
         }
-    });
-};
+    }.bind(this));
+}
--- items/NumberItem.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var request = require("request");
-
-var NumberItem = function(widget,platform,homebridge) {
-    NumberItem.super_.call(this, widget,platform,homebridge);
-};
-
-module.exports = NumberItem;
